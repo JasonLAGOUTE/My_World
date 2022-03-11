f9144170c134e975c861a16ee4341e83ca5f07d9
RoheeAxel <axel.rohee@epitech.eu>
2022-03-11 15:41:14 +0100

    Feat (draw.c): added cube shape border

--- src/map/draw.c
+++ src/map/draw.c
@@ -101,6 +101,7 @@ static void condit(map_t *map, sfRenderWindow *window, int i, int j)
 
 void draw_map(sfRenderWindow *window, map_t *map)
 {
+    
     for (int i = 0; i < MAP_X; i++) {
         for (int j = 0; j < MAP_Y; j++) {
             map->points3d = (sfVector3f){i, j, map->map[i][j]};
@@ -118,5 +119,41 @@ void draw_map(sfRenderWindow *window, map_t *map)
     for (int i = 0; i < LAST_BARRE; i++) {
         sfRenderWindow_drawRectangleShape(window, map->buttons->tab_textures[i]->rect, NULL);
         sfRenderWindow_drawText(window, map->buttons->tab_textures[i]->text, NULL);
+    }
+    for (int i = 0; i < MAP_X - 1; i++) {
+        map->points3d = (sfVector3f){i, MAP_X - 1, map->map[i][MAP_X - 1]};
+        map->points = to2d(map->points3d, map);
+        map->points3d = (sfVector3f){i + 1, MAP_X - 1, map->map[i + 1][MAP_X - 1]};
+        map->points2 = to2d(map->points3d, map);
+        map->points3d = (sfVector3f){i, MAP_X - 1, 0};
+        map->points3 = to2d(map->points3d, map);
+        map->points3d = (sfVector3f){i + 1, MAP_X - 1, 0};
+        map->points4 = to2d(map->points3d, map);
+        map->points3.y+= MAP_X * 3/4 * map->cam.zoom;
+        map->points4.y+= MAP_X * 3/4 * map->cam.zoom;
+        sfRenderWindow_drawVertexArray(window,
+            create_triangle_left(&map->points, &map->points3, &map->points2, (sfColor){178, 78, 45, 255}),
+            NULL);
+        sfRenderWindow_drawVertexArray(window,
+            create_triangle_left(&map->points2, &map->points4, &map->points3, (sfColor){178, 78, 45, 255}),
+            NULL);
+    }
+    for (int i = 0; i < MAP_X - 1; i++) {
+        map->points3d = (sfVector3f){MAP_X - 1, i, map->map[MAP_X - 1][i]};
+        map->points = to2d(map->points3d, map);
+        map->points3d = (sfVector3f){MAP_X - 1, i + 1, map->map[MAP_X - 1][i+1]};
+        map->points2 = to2d(map->points3d, map);
+        map->points3d = (sfVector3f){MAP_X - 1, i, 0};
+        map->points3 = to2d(map->points3d, map);
+        map->points3d = (sfVector3f){MAP_X - 1, i + 1, 0};
+        map->points4 = to2d(map->points3d, map);
+        map->points3.y+= MAP_X * 3/4 * map->cam.zoom;
+        map->points4.y+= MAP_X * 3/4 * map->cam.zoom;
+        sfRenderWindow_drawVertexArray(window,
+            create_triangle_left(&map->points, &map->points3, &map->points2, (sfColor){197, 96, 62, 255}),
+            NULL);
+        sfRenderWindow_drawVertexArray(window,
+            create_triangle_left(&map->points2, &map->points4, &map->points3, (sfColor){197, 96, 62, 255}),
+            NULL);
     }
 }
