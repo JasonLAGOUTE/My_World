93ef9bdf1019fadc44474efe34661ceadfb077ce
RoheeAxel <axel.rohee@epitech.eu>
2022-03-14 15:28:00 +0100

    Fix (draw.c): segfault

--- src/map/draw/paint.c
+++ src/map/draw/paint.c
@@ -10,7 +10,7 @@
 void draw_button_paint(map_t *m, sfRenderWindow *w)
 {
     if (m->bool_barre == 1) {
-        for (int i = 0; i < LAST; i++) {
+        for (int i = 0; i < LAST - 1; i++) {
             sfRenderWindow_drawRectangleShape(w, m->btn_t->tab[i]->rec, NULL);
             sfRenderWindow_drawText(w, m->btn_t->tab[i]->text, NULL);
         }
