916d9b8e2c1a253fd66b15613105556b148d0759
Jason Lagoute <jason.lagoute@epitech.eu>
2022-03-17 00:24:50 +0100

    style(hover clique button)

--- src/button/render_tools.c
+++ src/button/render_tools.c
@@ -7,7 +7,40 @@
 
 #include "my_world.h"
 
-static void condit(window_t *window, sfMouseButtonEvent coord, map_t *map)
+static void disp_light(window_t *window, sfMouseButtonEvent coord, map_t *map)
+{
+    if (((coord.x >= 1740 && coord.x <= 1900) &&
+        (coord.y >= 731 && coord.y <= 804)) && window->menu == false) {
+        if (map->bool_barre == 3 && map->bool_r->light == 0) {
+            map->bool_r->light = 1;
+            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_LIGHT]->rec,
+                sfWhite);
+        } else if (map->bool_barre == 3 && map->bool_r->light == 1) {
+            map->bool_r->light = 0;
+            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_LIGHT]->rec,
+                (sfColor){0, 255, 40, 255});
+        }
+    }
+}
+
+static void disp_cube(window_t *window, sfMouseButtonEvent coord, map_t *map)
+{
+    if (((coord.x >= 1740 && coord.x <= 1900) &&
+        (coord.y >= 575 && coord.y <= 648)) && window->menu == false) {
+        if (map->bool_barre == 3 && map->bool_r->cube == 0) {
+            map->bool_r->cube = 1;
+            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_CUBE]->rec,
+                sfWhite);
+        } else if (map->bool_barre == 3 && map->bool_r->cube == 1) {
+            map->bool_r->cube = 0;
+            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_CUBE]->rec,
+                (sfColor){0, 255, 40, 255});
+        }
+    }
+}
+
+static void disp_light_cube_texture(window_t *window,
+    sfMouseButtonEvent coord, map_t *map)
 {
     if (((coord.x >= 1740 && coord.x <= 1900) &&
         (coord.y >= 419 && coord.y <= 492)) && window->menu == false) {
@@ -20,26 +53,22 @@ static void condit(window_t *window, sfMouseButtonEvent coord, map_t *map)
             sfRectangleShape_setFillColor(map->btn_r->tab[DISP_TXT]->rec,
                 (sfColor){0, 255, 40, 255});
         }
-    } if (((coord.x >= 1740 && coord.x <= 1900) &&
-        (coord.y >= 575 && coord.y <= 648)) && window->menu == false) {
-        if (map->bool_barre == 3 && map->bool_r->cube == 0) {
-            map->bool_r->cube = 1;
-            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_CUBE]->rec,
+    }
+    disp_cube(window, coord, map);
+    disp_light(window, coord, map);
+}
+
+static void disp_point(window_t *window, sfMouseButtonEvent coord, map_t *map)
+{
+    if (((coord.x >= 1740 && coord.x <= 1900) &&
+        (coord.y >= 263 && coord.y <= 336)) && window->menu == false) {
+        if (map->bool_barre == 3 && map->bool_r->point == 1) {
+            map->bool_r->point = 0;
+            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_POINT]->rec,
                 sfWhite);
-        } else if (map->bool_barre == 3 && map->bool_r->cube == 1) {
-            map->bool_r->cube = 0;
-            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_CUBE]->rec,
-                (sfColor){0, 255, 40, 255});
-        }
-    } if (((coord.x >= 1740 && coord.x <= 1900) &&
-        (coord.y >= 731 && coord.y <= 804)) && window->menu == false) {
-        if (map->bool_barre == 3 && map->bool_r->light == 0) {
-            map->bool_r->light = 1;
-            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_LIGHT]->rec,
-                sfWhite);
-        } else if (map->bool_barre == 3 && map->bool_r->light == 1) {
-            map->bool_r->light = 0;
-            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_LIGHT]->rec,
+        } else if (map->bool_barre == 3 && map->bool_r->point == 0) {
+            map->bool_r->point = 1;
+            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_POINT]->rec,
                 (sfColor){0, 255, 40, 255});
         }
     }
@@ -58,17 +87,7 @@ void button_rander_tool(window_t *window, sfMouseButtonEvent coord, map_t *map)
             sfRectangleShape_setFillColor(map->btn_r->tab[DISP_LINE]->rec,
                 (sfColor){0, 255, 40, 255});
         }
-    } if (((coord.x >= 1740 && coord.x <= 1900) &&
-        (coord.y >= 263 && coord.y <= 336)) && window->menu == false) {
-        if (map->bool_barre == 3 && map->bool_r->point == 1) {
-            map->bool_r->point = 0;
-            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_POINT]->rec,
-                sfWhite);
-        } else if (map->bool_barre == 3 && map->bool_r->point == 0) {
-            map->bool_r->point = 1;
-            sfRectangleShape_setFillColor(map->btn_r->tab[DISP_POINT]->rec,
-                (sfColor){0, 255, 40, 255});
-        }
     }
-    condit(window, coord, map);
+    disp_point(window, coord, map);
+    disp_light_cube_texture(window, coord, map);
 }
