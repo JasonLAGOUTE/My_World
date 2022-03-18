49d42eabacea86d2c9473d211668653941c09c84
RoheeAxel <axel.rohee@epitech.eu>
2022-03-19 00:46:34 +0100

    Feat (read_map.c): load map event if map doesnt exist

--- src/map/read_map.c
+++ src/map/read_map.c
@@ -7,10 +7,24 @@
 
 #include "my_world.h"
 
+void zero_map(map_t *map)
+{
+    for (int i = 0; i < MAP_X; i++) {
+        for (int j = 0; j < MAP_Y; j++) {
+            map->map[i][j] = 0;
+            map->texture_map[i][j] = 0;
+        }
+    }
+}
+
 void load_map(map_t *map)
 {
     FILE* input;
     input = fopen("Save.map", "rb");
+    if (input == NULL ) {
+        zero_map(map);
+        return;
+    }
     for (int i = 0; i < MAP_X; i++) {
         for (int j = 0; j < MAP_Y; j++) {
             fread(&map->map[i][j], sizeof(int), 1, input);
